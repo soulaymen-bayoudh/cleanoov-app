84a4637522b59478b4b16923389e39570fdb8808
soulaymen <soulaymenbayoudh9@gmail.com>
2026-06-19 13:35:31 +0200

    Fix package name mismatch + minSdk/targetSdk for Android 15
    - MainActivity.kt moved to correct package com.cleanoov.cleanoovapp
    - minSdk = 21, targetSdk = 35 (Android 15 compatible)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.cleanoov.cleanoovapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 21
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
